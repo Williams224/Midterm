35e11bb6013cd6893304b5fc4308f28a9d46c904
Tim Williams <timothy.williams@cern.ch>
2015-08-18 11:39:14 +0100

    before changing shower profiles

--- Plotting/MakePlots.cpp
+++ Plotting/MakePlots.cpp
@@ -2,8 +2,8 @@
 #include"SixSimRun.hpp"
 #include"Comparison.hpp"
 int main(){
-  FiveSimRun* FiveOpt1= new FiveSimRun("/Users/Tim/PhD/Sim/MidTerm/Data/TestEm3/95/LHCbOld_Save95.root","v9.5 opt1");
-  SixSimRun* SixOpt1= new SixSimRun("/Users/Tim/PhD/Sim/MidTerm/Data/TestEm3/LHCbNew_Save.root","v9.6 opt1");
+  FiveSimRun* FiveOpt1= new FiveSimRun("/Users/Tim/PhD/Sim/MidTerm/Data/TestEm3/95/LHCbOld_Save95.root","v9.5 LHCb Private PL");
+  SixSimRun* SixOpt1= new SixSimRun("/Users/Tim/PhD/Sim/MidTerm/Data/TestEm3/LHCbNew_Save.root","v9.6 LHCb Private PL");
   Comparison *TheComparison= new Comparison("StraightComparison");
   TheComparison->AddSimRun(SixOpt1);
   TheComparison->AddSimRun(FiveOpt1);
